d199f323ef70d35f7ddd08f45cdf1326bf65e4bd
nightstarlabs <hello.nightstarlabs@gmail.com>
2026-05-19 00:24:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const revealElements = document.querySelectorAll(".reveal");
 
+const revealOnScroll = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+revealElements.forEach(element => {
+  revealOnScroll.observe(element);
+});
